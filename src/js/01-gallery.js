87afb1c24c01a7a904ecdacb1a00a93dd0d01105
Oleksandr Volik <polskasasha@gmail.com>
2022-07-22 15:30:42 +0300

    update-gallery-01-js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,9 +2,8 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const galleryContainer = document.querySelector('.gallery');
 
 const imagesMarkup = createGalleryItems(galleryItems);
