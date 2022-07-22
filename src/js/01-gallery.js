ebed577520c6804d370e11df011a56e27afd88e7
Oleksandr Volik <polskasasha@gmail.com>
2022-07-22 14:33:59 +0300

    make-01-task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+const imagesMarkup = createGalleryItems(galleryItems);
+galleryContainer.innerHTML = imagesMarkup;
+
+function createGalleryItems(image) {
+  return image
+    .map(({ preview, original, description }) => {
+      return `<li>
+        <a class = "gallery__item" href ="${original}">
+        <img class = "gallery__image"
+        src = "${preview}";
+        data-source = "${original}";
+        alt = "${description}";
+        />
+        </a>
+        </li>`;
+    })
+    .join('');
+}
+
+const imageContainer = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  animationSpeed: 250,
+});
 
 console.log(galleryItems);
