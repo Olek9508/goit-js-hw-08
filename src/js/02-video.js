31299e928e58c8053879eef3de115815495bc228
Oleksandr Volik <polskasasha@gmail.com>
2022-07-27 13:19:41 +0300

    do-tasks-02-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const videoEl = document.querySelector('#vimeo-player');
+const player = new Player(videoEl);
+const getTime = localStorage.getItem('videoplayer-current-time');
+
+function onSaveVideoTime(data) {
+  const dataTime = data.seconds;
+  console.log(dataTime);
+  localStorage.setItem('videoplayer-current-time', dataTime);
+}
+
+player.on('timeupdate', throttle(onSaveVideoTime, 1000));
+
+player
+  .setCurrentTime(getTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
